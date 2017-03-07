c60a9d62e4f8e2af4295d0c1d981025e1bcc7391
henry190794 <jhenrique1933@gmail.com>
2017-03-07 16:41:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
 			</td>
 			<td>
 				<h1 id = "message"><?php echo "Jose henrique"; ?></h1>
+				<form method="POST" action="./">
+   					 <input type="text" name="nome" />
+   					 <input type="submit" value="Cadastrar" />
+				</form>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
